5012b8c46c4709e98b12994f8939083bb9a4cbee
senenkovitalik <senenkovitalik@gmail.com>
2020-03-27 12:30:16 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,44 +10,44 @@ import { UserModel } from './types';
 export class AppComponent  {
   @ViewChild('modal') modal: any;
 
-  // private users: UserModel[] = []; [
-  //   {
-  //     username: 'Vlad',
-  //     description: 'Worker',
-  //     comment: 'Work all day long',
-  //     password: '734gfg87g34',
-  //     isActive: true,
-  //     createdAt: '01.05.2018',
-  //     updatedAt: '04.02.2019'
-  //   },
-  //   {
-  //     username: 'Nelya',
-  //     description: 'Reception',
-  //     comment: 'Stupid bitch',
-  //     password: '12345',
-  //     isActive: false,
-  //     createdAt: '07.02.2015',
-  //     updatedAt: '14.22.2016'
-  //   },
-  //   {
-  //     username: 'Ivan',
-  //     description: 'IT',
-  //     comment: 'Long cable',
-  //     password: '329t4o8ug34t4865',
-  //     isActive: true,
-  //     createdAt: '11.11.2018',
-  //     updatedAt: '04.02.2020'
-  //   },
-  //   {
-  //     username: 'Oleg',
-  //     description: 'Manager',
-  //     comment: 'Work all day long',
-  //     password: '234RG346H#$T',
-  //     isActive: true,
-  //     createdAt: '01.05.2018',
-  //     updatedAt: '04.02.2019'
-  //   },
-  // ];
+  private users: UserModel[] = [
+    {
+      username: 'Vlad',
+      description: 'Worker',
+      comment: 'Work all day long',
+      password: '734gfg87g34',
+      isActive: true,
+      createdAt: '01.05.2018',
+      updatedAt: '04.02.2019'
+    },
+    {
+      username: 'Nelya',
+      description: 'Reception',
+      comment: 'Stupid bitch',
+      password: '12345',
+      isActive: false,
+      createdAt: '07.02.2015',
+      updatedAt: '14.22.2016'
+    },
+    {
+      username: 'Ivan',
+      description: 'IT',
+      comment: 'Long cable',
+      password: '329t4o8ug34t4865',
+      isActive: true,
+      createdAt: '11.11.2018',
+      updatedAt: '04.02.2020'
+    },
+    {
+      username: 'Oleg',
+      description: 'Manager',
+      comment: 'Work all day long',
+      password: '234RG346H#$T',
+      isActive: true,
+      createdAt: '01.05.2018',
+      updatedAt: '04.02.2019'
+    },
+  ];
 
   private expandedUser: UserModel;
   private userToDelete: UserModel;
